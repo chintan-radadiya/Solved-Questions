955d9ab8f94422d3a3f1a25207d0a9e61d1e1a14
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-09 00:11:06 +0530

    First commit

---
+++ leap_year.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class leap_year {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a year:");
+        int year = sc.nextInt();
+
+
+        if(year % 4 == 0){
+            if(year % 100 == 0){
+                if(year % 400 == 0){
+                    System.out.println("Leap year");
+                }else{
+                    System.out.println("Not a leap year");
+                }
+            }else{
+                System.out.println("Leap year");
+            }
+
+        }else{
+            System.out.println("Not Leap year");
+        }
+    }
+}
