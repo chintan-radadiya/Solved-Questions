35d1ee4d7322eb8f06b201beb354043d2e5f2128
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-09 00:12:57 +0530

    First commit

---
+++ sum_of_digits.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class sum_of_digits {
+
+    static int sumOfDigit(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int lastdigit = n % 10;
+            sum += lastdigit;
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+
+        System.out.println(sumOfDigit(num));;
+    }
+}
