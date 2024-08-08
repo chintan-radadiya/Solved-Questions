00f116a6a63840bedd670bcea482eb2c766be36b
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-09 00:11:58 +0530

    First Commit

---
+++ prime_number.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class prime_number {
+
+    static void isPrime(int n){
+
+        boolean isprime = true;
+
+        for (int i = 2; i <= Math.sqrt(n) ; i++) {
+
+            if(n % i == 0){
+                isprime = false;
+                break;
+            }
+        }
+
+        if(isprime == true){
+            System.out.println("Number is Prime.");
+        }else{
+            System.out.println("Number is not Prime");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+       System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        isPrime(num);
+
+    }
+}
