756bce9e16b32f0db71929a4a301ec8ae641ce41
chintan-radadiya <chintan96radadiya@gmail.com>
2024-08-10 22:13:32 +0530

    First commit

---
+++ Armstrong_number.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Armstrong_number {
+
+    static int digit(int n){
+        int count = 0;
+
+        while(n > 0){
+
+            n = n / 10;
+            count++;
+        }
+        return count;
+    }
+
+    static boolean isArmstrong(int n) {
+    
+        double sum = 0;
+        int power = digit(n);
+
+        int temp = n;
+
+        boolean result = false;
+
+        while(temp > 0){
+            int last_digit = temp % 10;
+            sum += (Math.pow(last_digit, power));
+            temp/=10;
+        }
+
+        if(sum == n){
+            result = true;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number to check:");
+        int num = sc.nextInt();
+        
+
+        System.out.println("Number is armstrong: " + isArmstrong(num));
+
+    }
+}
